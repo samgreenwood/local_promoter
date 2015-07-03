40db4c488abc0eb8fd701150f8aec88e7b149871
David Smith <david@codium.com.au>
2015-07-04 09:15:52 +0930

    - updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine='InnoDB';
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
+            $table->integer('company_id')->nullable()->unsigned();
+            $table->integer('suburb_id')->nullable()->unsigned();
+            $table->integer('gender')->nullable();
+            $table->string('firstName', 45);
+            $table->string('surname', 100);
+            $table->string('street', 255)->nullable();
             $table->string('password', 60);
+            $table->date('dob')->nullable();
+            $table->string('phone', 45)->nullable();
+            $table->string('mobile', 45)->nullable();
+            $table->string('facebook_id', 45)->nullable();
+            $table->string('image')->nullable();
+            $table->string('aboutMe')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
