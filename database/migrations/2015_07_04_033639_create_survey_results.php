a9196579a4eb9522f890d3c91f29c5fd29e2f0f1
Jason Smith <jason@allmyit.com.au>
2015-07-04 13:34:16 +0930

    - database

---
+++ database/migrations/2015_07_04_033639_create_survey_results.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SurveyResults extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_results', function($table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->string('rating');
+            $table->string('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('survey_results');
+    }
+}
