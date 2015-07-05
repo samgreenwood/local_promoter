4ee315ccdb21ea1e17fc461055d124f764147477
Sam Greenwood <sam@goonbook.local>
2015-07-05 16:45:13 +0930

    check for authed user

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -127,7 +127,7 @@ class CompanyController extends Controller
     {
         $company = Company::find($companyId);
 
-        if(auth()->user->company)
+        if(auth()->user()->company)
         {
             if (auth()->user()->company && auth()->user()->company->id != $companyId && $company->tourism_id == '') {
                 return redirect('/');
